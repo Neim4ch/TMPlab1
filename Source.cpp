e3fb15f85f3cca43723ef28351acb6b09bf59505
Neim4ch <64603880+Neim4ch@users.noreply.github.com>
2022-02-09 19:42:04 +0700

    list+films+OOP
    123

--- Source.cpp
+++ Source.cpp
@@ -1,57 +1,123 @@
 #include "Source.h"
 using namespace std; 
-void rectangle::InData(ifstream& ifst) {
-	ifst >> x >> y;
+void feature::InData(ifstream& ifst) {
+	ifst >> director;
 }
-void rectangle::Out(ofstream& ofst) {
-	ofst << "It is Rectangle: x = " << x
-		 << ", y = " << y << endl;
+void feature::Out(ofstream& ofst) {
+	ofst << "It is feature film. Director is " << director << endl;
 }
-void triangle::InData(ifstream& ifst) {
-	ifst >> a >> b >> c;
+void animation::InData(ifstream& ifst) {
+	int t;
+	ifst >> t;
+	switch (t)
+	{
+	case 1:
+		woc = DRAWN;
+		break;
+	case 2:
+		woc = DOLL;
+		break;
+	case 3:
+		woc = STOP_MOTION;
+		break;
+	}
 }
-void triangle::Out(ofstream& ofst) {
-	ofst << "It is Triangle: a = "
-		<< a << ", b = " << b
-		<< ", c = " << c << endl;
+void animation::Out(ofstream& ofst) {
+	switch (woc)
+	{
+	case 0:
+		//woc = DRAWN;
+		ofst << "It is animation film. It's way of creation is drawing."<< endl;
+		break;
+	case 1:
+		//woc = DOLL;
+		ofst << "It is animation film. It's way of creation is using dolls" << endl;
+		break;
+	case 2:
+		//woc = STOP_MOTION;
+		ofst << "It is animation film. It's way of creation is stop motion" << endl;
+		break;
+	}
+
+	//ofst << "It is animation film. It's way of creation is " << woc << endl;
 }
-shape* shape::In(ifstream& ifst) {
-	shape* sp;
+film* film::In(ifstream& ifst) {
+	film* fl;
 	int k;
 	ifst >> k;
 	switch (k) {
 	case 1:
-		sp = new rectangle;
+		fl = new feature;
 		break;
 	case 2:
-		sp = new triangle;
+		fl = new animation;
 		break;
 	default:
 		return 0;
 	}
-	sp->InData(ifst);
-	return sp;
+	fl->InData(ifst);
+	return fl;
 }
-container::container() : len(0) { }
+Node::Node(film* newpic)
+{
+	pic = newpic;
+	next = NULL;
+}
+container::container()
+{
+	head = NULL;
+	head = NULL;
+	size = 0;
+}
 // Очистка контейнера от элементов
 void container::Clear() {
-	for (int i = 0; i < len; i++) {
-		delete cont[i];
-	}
-	len = 0;
+	head = NULL;
+	curr = NULL;
+	size = 0;
 }
+//Node::Node()
+//{
+//	figure = new shape;
+//	next = NULL;
+//}
 void container::In(ifstream& ifst) {
 	while (!ifst.eof()) {
-		if ((cont[len] = shape::In(ifst)) != 0) {
+		/*if ((cont[len] = shape::In(ifst)) != 0) {
 			len++;
+		}*/
+		Node* newNode;
+		newNode = new Node((film::In(ifst)));
+		if (head == NULL)
+		{
+			head = newNode;
+			size = 1;
+		}
+		else
+		{
+			curr = head;
+			while (curr->next != NULL)
+			{
+				curr = curr->next;
+			}
+			curr->next = newNode;
+			size++;
 		}
 	}
 }
 void container::Out(ofstream& ofst) {
-	ofst << "Container contents " << len
+	ofst << "Container contents " << size
 		<< " elements." << endl;
-	for (int i = 0; i < len; i++) {
+	/*for (int i = 0; i < size; i++) {
 		ofst << i << ": ";
 		cont[i]->Out(ofst);
+	}*/
+	int i = 0;
+	curr = head;
+	while (curr != NULL)
+	{
+		ofst << i << ": ";
+		curr->pic->Out(ofst);
+		curr = curr->next;
+		i++;
 	}
 }
--- Source.h
+++ Source.h
@@ -2,47 +2,54 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-class shape {
+class film {
 public:
 	// иденитфикация, порождение и ввод фигуры из потока
-	static shape* In(ifstream& ifst);
+	static film* In(ifstream& ifst);
 	virtual void InData(ifstream& ifst) = 0; // ввод
 	virtual void Out(ofstream& ofst) = 0; // вывод
 };
-// прямоугольник
-class rectangle : public shape {
-	int x, y; // ширина, высота
+class Node {
 public:
-	// переопределяем интерфейс класса
-	void InData(ifstream& ifst); // ввод
-	void Out(ofstream& ofst); // вывод
-	rectangle() {} // создание без инициализации.
+	film* pic;
+	Node* next;
+	Node(film* newfigure);
+	//~Node() {}
 };
-// треугольник
-class triangle : public shape {
-	int a, b, c; // стороны
+// игровой фильм
+class feature : public film {
+	string director; // режисер
 public:
 	// переопределяем интерфейс класса
 	void InData(ifstream& ifst); // ввод
 	void Out(ofstream& ofst); // вывод
-	triangle() {} // создание без инициализации.
+	feature() {} // создание без инициализации.
+};
+// мультфильм
+class animation : public film {
+	enum way { DRAWN, DOLL, STOP_MOTION };// способ создания
+	way woc;//способ создания
+public:
+	// переопределяем интерфейс класса
+	void InData(ifstream& ifst); // ввод
+	void Out(ofstream& ofst); // вывод
+	animation() {} // создание без инициализации.
 };
 class container {
 	//enum { max_len = 100 }; // максимальная длина
 	//int len; // текущая длина
 	//shape* cont[max_len];
-	Node* cont;
-	Node* curr;
+	
 public:
+	Node *head;
+	Node *curr;
+	int size;
+
 	void In(ifstream& ifst); // ввод
 	void Out(ofstream& ofst); // вывод
 	void Clear(); // очистка контейнера от фигур
 	container(); // инициализация контейнера
 	~container() { Clear(); } // утилизация контейнера
 };
-class Node {
-	shape* figure;
-	Node* next;
-	Node(shape* newfigure);
-};
+
 
